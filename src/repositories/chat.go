834b26ccdc5c40f89e5d631ac22617005b3bd98f
Akihira77 <tik.dikawahyu123@gmail.com>
2025-03-02 08:00:03 +0700

    feat: refactore retrieving chat list query

--- src/repositories/chat.go
+++ src/repositories/chat.go
@@ -75,29 +75,64 @@ func (cr *ChatRepository) SearchChat(ctx context.Context, myUserId, groupName, u
 		store.
 		DB.
 		Debug().
-		Model(&types.Group{}).
 		WithContext(ctx).
-		Joins("LEFT JOIN user_groups ON user_groups.group_id = groups.id").
-		Joins("LEFT JOIN messages ON messages.group_id = groups.id").
-		Joins("LEFT JOIN users ON users.id = messages.sender_id").
-		Select(
-			"(CASE WHEN groups.name IS NULL THEN users.id ELSE NULL END) AS sender_id",
-			"(CASE WHEN groups.name IS NULL THEN users.first_name || ' ' || users.last_name ELSE NULL END) AS user_name",
-			"(CASE WHEN groups.name IS NULL THEN users.image_url ELSE NULL END) AS user_profile",
-			"COUNT(CASE WHEN messages.is_read = 0 AND messages.sender_id = users.id AND messages.receiver_id IS NOT NULL THEN 1 END) AS unread_peer_chat",
-			"groups.id AS group_id",
-			"groups.name AS group_name",
-			"groups.group_profile",
-			"COUNT(CASE WHEN messages.is_read = 0 AND messages.group_id = groups.id AND messages.group_id IS NOT NULL THEN 1 END) AS unread_group_chat",
-		).
-		Where(
-			"(messages.receiver_id = ? AND (users.first_name || ' ' || users.last_name) LIKE ?) OR groups.name LIKE ?",
-			myUserId,
+		Raw(
+			`
+            SELECT
+                (CASE WHEN messages.group_id IS NULL THEN users.id ELSE NULL END) AS sender_id,
+                (CASE WHEN messages.group_id IS NULL THEN users.first_name || ' ' || users.last_name ELSE NULL END) AS user_name,
+                (CASE WHEN messages.group_id IS NULL THEN users.image_url ELSE NULL END) AS user_profile,
+                (CASE WHEN messages.group_id IS NULL THEN users.status ELSE NULL END) AS user_status,
+                COUNT(CASE WHEN messages.is_read = 0 AND messages.sender_id = users.id AND messages.receiver_id IS NOT NULL THEN 1 END) AS unread_peer_chat,
+                groups.id AS group_id,
+                groups.name AS group_name,
+                groups.group_profile,
+                COUNT(CASE WHEN messages.is_read = 0 AND messages.group_id = groups.id AND messages.receiver_id IS NULL THEN 1 END) AS unread_group_chat,
+                messages.content,
+                messages.created_at AS created_at
+            FROM messages
+            LEFT JOIN users ON users.id = messages.sender_id
+            LEFT JOIN groups ON groups.id = messages.group_id
+            LEFT JOIN user_groups ON user_groups.group_id = messages.group_id
+                WHERE
+                    ((users.first_name || ' ' || users.last_name) LIKE ? OR groups.name LIKE ?) AND
+                    messages.sender_id <> ? AND 
+                    (messages.receiver_id = ? OR user_groups.user_id = ?)
+                GROUP BY users.id, groups.id
+            UNION
+            SELECT
+                NULL AS sender_id,
+                NULL AS user_name,
+                NULL AS user_profile,
+                NULL AS user_status,
+                0 AS unread_peer_chat,
+                groups.id AS group_id,
+                groups.name AS group_name,
+                groups.group_profile,
+                0 AS unread_group_chat,
+                NULL,
+                groups.created_at AS created_at
+            FROM groups
+            LEFT JOIN user_groups ON user_groups.group_id = groups.id
+            LEFT JOIN users ON users.id = user_groups.user_id
+                WHERE
+                    groups.name LIKE ? AND
+                    groups.id NOT IN (
+                        SELECT DISTINCT group_id FROM messages
+                        WHERE group_id IS NOT NULL
+                    ) AND
+                    user_groups.user_id = ?
+                GROUP BY sender_id, group_id
+                ORDER BY created_at DESC
+        `,
 			userName,
 			groupName,
+			myUserId,
+			myUserId,
+			myUserId,
+			groupName,
+			myUserId,
 		).
-		Group("users.id, groups.id").
-		Order("MAX(messages.created_at) DESC").
 		Find(&chatHistories)
 
 	return chatHistories, res.Error
